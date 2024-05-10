ce4d3a7b66c06c22340db94fe6fa2d478b054307
irfan786-ops <67737732+irfan786-ops@users.noreply.github.com>
2024-05-10 17:47:01 +0530

    dashboard

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './shared/components/header/header/header.component';
+import { LeftNavigationComponent } from './shared/components/left_Navigation/left-navigation/left-navigation.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,HeaderComponent,LeftNavigationComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
